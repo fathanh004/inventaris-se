d91c4d3af5497377a1daa3af2085c5f6f4b93ef2
fathanh004 <fathanhidayatullah.2020@student.uny.ac.id>
2022-10-25 09:33:00 +0700

    Edit css dan route

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,6 +9,7 @@ class Home extends BaseController
     function __construct()
     {
         $this->user_model = new user_model();
+        
     }
 
     public function index()
@@ -21,6 +22,7 @@ class Home extends BaseController
 
     function auth()
     {
+    
         $username = $this->request->getPost('username');
         $password = $this->request->getPost('password');
 
@@ -28,7 +30,7 @@ class Home extends BaseController
         $cek_admin = $this->user_model->where($array)->findAll();
 
         if ($cek_admin != null) {
-            redirect('/crudcontroller/tampil_barang');
+            return redirect()->route('barang');
             // $this->session->set_userdata('masuk', TRUE);
             // $this->session->set_userdata('ses_id', $data['user_id']);
             // redirect('pageAdmin');
@@ -36,7 +38,7 @@ class Home extends BaseController
             $array1 = array('username' => $username, 'password' => $password, 'role' => 'employee');
             $cek_employee = $this->user_model->where($array1)->findAll();
             if ($cek_employee != null) {
-                redirect('/crudcontroller/tampil_barang');
+                return redirect()->route('barang');
                 // $this->session->set_userdata('masuk', TRUE);
                 // $this->session->set_userdata('ses_id', $data['user_id']);
                 // redirect('pageEmployee');
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -22,6 +22,8 @@
 
 <?= $this->renderSection('content'); ?>
 
+
+
 <!-- Bootstrap core JavaScript-->
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
